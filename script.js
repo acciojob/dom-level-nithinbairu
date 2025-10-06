e656297ca67d2886daede788ed0d3b8fb4b208f7
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-10-06 15:21:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const listItems = document.querySelectorAll("ul li");
+
+// Convert NodeList to array and find index of item with an id
+const index = Array.from(listItems).findIndex(item => item.id);
+alert("The level of th element is: "+index)
